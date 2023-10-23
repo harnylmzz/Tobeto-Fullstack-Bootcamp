b905dae8461e179123da44b0f25ef9948b0c14dd
harnylmzz <harnylmzzonline@gmail.com>
2023-10-23 20:15:50 +0300

    OOP1

--- Kamp2-OOP1/src/main/java/org/example/Main.java
+++ Kamp2-OOP1/src/main/java/org/example/Main.java
@@ -3,10 +3,53 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        // Bireysel müşteri oluşturuyoruz
+        IndividualCustomer individualCustomer = new IndividualCustomer();
+        individualCustomer.setId(1);
+        individualCustomer.setCustomerNumber("12345678910");
+        individualCustomer.setFirstName("John");
+        individualCustomer.setLastName("Doe");
+        individualCustomer.setPhone("0523697846541");
+
+        // Kurumsal müşteri oluşturuyoruz
+        CorporateCustomer corporateCustomer = new CorporateCustomer();
+        corporateCustomer.setCustomerNumber("78945612");
+        corporateCustomer.setCompanyName("Batarya Company");
+        corporateCustomer.setTaxNumber("1236547891");
+        corporateCustomer.setPhone("0578945612");
+
+        Customer[] customers = {individualCustomer, corporateCustomer};
+        for (Customer customer : customers) {
+            System.out.println(customer.getCustomerNumber());
+        }
+
         // Ürünleri oluşturuyoruz
-        Product product1 = new Product();
-        Product product2 = new Product();
-        Product product3 = new Product();
+        Product product1 = new Product("Delongi Kahve Makinesi", 7500, 3, "image.jpg", 3);
+        Product product2 = new Product("Arçelik Kahve Makinesi", 6500, 5, "image2.url", 5);
+        Product product3 = new Product("Kitchen Kahve Makinesi", 3500, 9, "image3.url", 7);
+
+        /*
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setDiscount(7500);
+        product1.setUnitInStock(3);
+        product1.setDiscount(7);
+        product1.setImageUrl("image.jpg");
+
+        product1.setName("Arçelik Kahve Makinesi");
+        product1.setDiscount(7500);
+        product1.setUnitInStock(3);
+        product1.setDiscount(7);
+        product1.setImageUrl("image2.jpg");
+
+        product1.setName("Kitchen Kahve Makinesi");
+        product1.setDiscount(7500);
+        product1.setUnitInStock(3);
+        product1.setDiscount(7);
+        product1.setImageUrl("image3.jpg");
+
+         */
+
+        /*
 
         // Ürün 1'in bilgilerini dolduruyoruz
         product1.name = "Delonghi Kahve Makinesi";
@@ -36,11 +79,14 @@ public class Main {
 
         System.out.println("-------------------------");
 
+        */
+
+
         //Array
         Product[] products = {product1, product2, product3};
 
         for (Product product : products) {
-            System.out.println(product.name);
+            System.out.println(product.getName());
 
         }
     }
--- Kamp2-OOP1/src/main/java/org/example/Product.java
+++ Kamp2-OOP1/src/main/java/org/example/Product.java
@@ -2,9 +2,59 @@ package org.example;
 
 public class Product {
 
-    String name;
-    double price;
-    double discount;
-    String imageUrl;
-    int unitInStock;
+    private String name;
+    private double price;
+    private double discount;
+    private String imageUrl;
+    private int unitInStock;
+
+
+    public Product(String name, double price, double discount, String imageUrl, int unitInStock) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.imageUrl = imageUrl;
+        this.unitInStock = unitInStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getUnitInStock() {
+        return unitInStock;
+    }
+
+    public void setUnitInStock(int unitInStock) {
+        this.unitInStock = unitInStock;
+    }
+
 }
